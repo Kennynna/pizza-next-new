aa83660e4d9bcb1a26dee5975003204420389516
Kennynna <kennynna@mail.ru>
2024-07-16 00:51:10 +0300

    initial and add ui components

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Привет Дуьне!</h1>
+      <Button variant={"outline"} className="ml-4">Кнопка</Button>
+    </div>
+  );
+}
